92fa4bcbd4706caa16cd0892f949afd963ead20a
nutaptap <nutaptap@gmail.com>
2024-02-17 18:02:01 +0100

    Initial setup and added routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { Header } from "./components/Header"
+import { TypingTest } from "./components/TypingTest"
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <Router>
+      <div className="font-cascadia-mono text-[#f1e2e4] bg-[#2b2b2c] h-screen">
+        <Header />
+        <Routes>
+          <Route path="/" Component={TypingTest} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App
---
+++ frontend/src/components/TypingTest.tsx
@@ -0,0 +1,3 @@
+export function TypingTest() {
+  return <p className="text-center my-16 text-slate-700">Typing game</p>
+}
