3e359bfc1082528037a8d5445cfcca459032e648
nutaptap <nutaptap@gmail.com>
2024-03-12 23:05:34 +0100

    Added cors fix

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require("cors")
 require("dotenv").config()
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const scoresRouter = require("./routes/scores")
 app.use("/scores", scoresRouter)
